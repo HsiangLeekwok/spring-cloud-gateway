137b852e21af2a8ced5054954ca9b3c99a019d04
Hsiang Leekwok <xfeiffer@hotmail.com>
2020-02-18 17:49:13 +0800

    custom filter

---
+++ src/main/java/com/leekwok/gateway/filter/MyLogGatewayFilterFactory.java
@@ -0,0 +1,30 @@
+package com.leekwok.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * <b>Author</b>: Hsiang Leekwok<br/>
+ * <b>Date</b>: 2020/02/18 17:30<br/>
+ * <b>Version</b>: v1.0<br/>
+ * <b>Subject</b>: <br/>
+ * <b>Description</b>:
+ */
+@Component
+public class MyLogGatewayFilterFactory extends AbstractNameValueGatewayFilterFactory {
+    public static final Logger LOGGER= LoggerFactory.getLogger(MyLogGatewayFilterFactory.class);
+    @Override
+    public GatewayFilter apply(NameValueConfig config) {
+        return (((exchange, chain) -> {
+            LOGGER.info("请求进来了，key={}, value={}",config.getName(),config.getValue());
+            ServerHttpRequest modifiedRequest = exchange.getRequest().mutate().build();
+            ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
+            return chain.filter(modifiedExchange);
+        }));
+    }
+}
